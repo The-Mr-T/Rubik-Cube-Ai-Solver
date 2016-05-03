9b725198a641d24af04ebeb03bc05b8524353b18
Laurent Tremblay <laurent.m.tremblay@gmail.com>
2016-05-03 16:53:08 -0400

    added basic terminal printing support

--- RubikCube.py
+++ RubikCube.py
@@ -3,6 +3,8 @@
 
 __author__ = 'laurenttremblay'
 
+import sys
+from termcolor import colored, cprint
 
 class RubikCube():
     """Represents a Rubik cube. Color of the faces are represented by strings
@@ -24,29 +26,97 @@ class RubikCube():
         self.bottomFace = [["O" for x in range(w)] for y in range(h)]
 
         self.faces = {'top': self.topFace,
-                 'front': self.frontFace,
-                 'back': self.backFace,
-                 'left': self.leftFace,
-                 'right': self.rightFace,
-                 'bottom': self.bottomFace}
+                      'front': self.frontFace,
+                      'back': self.backFace,
+                      'left': self.leftFace,
+                      'right': self.rightFace,
+                      'bottom': self.bottomFace}
 
+        self.color = {'R': 'red',
+                      'B': 'blue' ,
+                      'G': 'green',
+                      'W': 'white',
+                      'O': 'cyan',
+                      'Y': 'yellow'}
 
-    def rotateTop(self, orientation):
+    #def rotateTop(self, orientation):
 
-    def rotateFront(self, orientation):
+    #def rotateFront(self, orientation):
 
-    def rotateBack(self, orientation):
+    #def rotateBack(self, orientation):
 
-    def rotateLeft(self, orientation):
+    #def rotateLeft(self, orientation):
 
-    def rotateRight(self, orientation):
+    #def rotateRight(self, orientation):
 
-    def rotateBottom(self, orientation):
+    #def rotateBottom(self, orientation):
 
-    def rotateMiddleVert(self, orientation):
+    #def rotateMiddleVert(self, orientation):
 
-    def rotateMiddleHori(self, orientation):
+    #def rotateMiddleHori(self, orientation):
 
-    def rotateSolidFace(self, face):
-        """Rotate a single flat face, not accounting for the cube's geometry"""
+    def rotateSolidFace(self, face, direction):
+        """Rotate a single flat face, not accounting for the cube's geometry
+
+        Returns the rotated face
+        """
+        # Borrowed to Peter Norvig http://www.norvig.com/python-iaq.html
+        # And http://stackoverflow.com/a/496056
+
+        if direction == "cw":
+            # Rotates Clockwise
+            return zip(*face[::-1])
+
+        elif direction == "ccw":
+            # Rotate counter-clockwise
+            return zip(*face)[::-1]
+
+        else:
+            raise NameError('NotAValidOrientation')
+            return
+
+def printCube(cube):
+    """Prints an unwrapped cube to the terminal, using ANSII codes"""
+    size = len(cube.faces['top'])
+#█
+    #Prints the first back cube
+    text = ""
+
+    for elem in range(size):
+        for x in range(size):
+            text = text + colored(' ', 'red')
+        for y in range(size):
+            text = text + colored('█', cube.color[cube.faces['back'][elem][y]] )
+        print(text)
+        text = ""
+
+    for elem in range(size):
+        #left
+        for y in range(size):
+            text = text + colored('█', cube.color[cube.faces['left'][elem][y]] )
+        #top
+        for y in range(size):
+            text = text + colored('█', cube.color[cube.faces['top'][elem][y]] )
+        #right
+        for y in range(size):
+            text = text + colored('█', cube.color[cube.faces['right'][elem][y]] )
+        #botom
+        for y in range(size):
+            text = text + colored('█', cube.color[cube.faces['bottom'][elem][y]] )
+        print(text)
+        text = ""
+
+    #Prints the last front cube
+    for elem in range(size):
+        for x in range(size):
+            text = text + colored(' ', 'red')
+        for y in range(size):
+            text = text + colored('█', cube.color[cube.faces['front'][elem][y]] )
+        print(text)
+        text = ""
+
+x = RubikCube()
+
+printCube(x)
+
 
